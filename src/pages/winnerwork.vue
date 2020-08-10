f1bc8c577eb0b8b785b3163ba3881a12551453aa
Artzy <pakkawat.kh@gmail.com>
2020-08-10 16:54:12 +0700

    update

--- src/pages/winnerwork.vue
+++ src/pages/winnerwork.vue
@@ -1,73 +1,21 @@
 <template>
   <q-page>
-    WORK
-    <div align="center">
-      <q-resize-observer @resize="onResize" />
-      <q-card v-if="innerWidth > 1023" style=";max-width: 1920px">
-        <div class="loginonet">
-          <div>
-            <div style="height:200px"></div>
-            <div class="relative-position">
-              <div class="absolute-bottom row justify-center q-pb-md">
-                <q-img src="../statics/image/loginonet/name-onet.png" style="max-width:700px;" />
-              </div>
+    <div class="row desktop-only" style="height:calc(100vh - 61px)">
+      <div class="col-6 bg-indigo-12"></div>
+      <div class="col-6">
+        <div>
+          <div class="row">
+            <div class="col-12">
+              <q-btn label="Staff Section" size="xl" color="indigo-12" style="width:300px;" />
             </div>
-            <div align="center">
-              <div class="row q-py-md" style="width:100%;max-width:1000px">
-                <div class="col-12 q-pt-xl">
-                  <a
-                    href="https://onetp6.winner-english.com/"
-                    target="_blank"
-                    style="text-decoration:none;"
-                  >
-                    <q-btn
-                      push
-                      class="q-my-sm btn bgbrown text-white"
-                      size="22px"
-                      label="WINNER O-NET ป.6"
-                    />
-                  </a>
-                </div>
-                <div class="col-12">
-                  <a
-                    href="https://o-net-review.web.app/"
-                    target="_blank"
-                    style="text-decoration:none;"
-                  >
-                    <q-btn
-                      push
-                      class="q-my-sm btn bgbrown text-white"
-                      size="22px"
-                      label="ทบทวน O-NET"
-                    />
-                  </a>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </q-card>
-      <div align="center" v-if="innerWidth < 1024" class="loginonet">
-        <div class="row justify-center q-pt-xl">
-          <q-img src="../statics/image/loginonet/name-onet.png" style="max-width:700px;" />
-        </div>
-        <div class="row q-pa-md">
-          <div class="col-12 q-pt-md">
-            <div>
-              <a
-                href="https://onetp6.winner-english.com/"
-                target="_blank"
-                style="text-decoration:none;"
-              >
-                <q-btn push class="q-my-sm btnmb bgbrown text-white" label="WINNER O-NET ป.6" />
-              </a>
-            </div>
-          </div>
-          <div class="col-12">
-            <div>
-              <a href="https://o-net-review.web.app/" target="_blank" style="text-decoration:none;">
-                <q-btn push class="q-my-sm btnmb bgbrown text-white" label="ทบทวน O-NET" />
-              </a>
+            <div class="col-12">
+              <q-btn
+                label="HR Section"
+                size="xl"
+                color="indigo-12"
+                style="width:300px;"
+                class="q-mt-md"
+              />
             </div>
           </div>
         </div>
