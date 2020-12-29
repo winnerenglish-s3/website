cbd6645bf79c2e7df93ec2b07490f0351c95e394
tatum <w.ntapong@gmail.com>
2020-12-29 10:31:00 +0700

    add page / business-businesshare-contact

---
+++ src/pages/businesshare.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="absolute-center">
+    <h1>BUSINESS SHARE</h1>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss" scoped></style>
---
+++ src/pages/contact.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="absolute-center"><h1>CONTACT US</h1></div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss" scoped></style>
