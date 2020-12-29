ec7bd781ce733ef90ea8c999c7905e73d09d0555
tatum <w.ntapong@gmail.com>
2020-12-29 16:36:18 +0700

    update footer

--- src/components/footer.vue
+++ src/components/footer.vue
@@ -12,18 +12,35 @@
               <span
                 class="text-white"
                 onclick="window.open('https://www.winner-english.com', '_blank')"
-              >www.winner-english.com</span>
+                >www.winner-english.com</span
+              >
             </span>
           </div>
-          <div>
-            <span class="q-pr-md">
-              <q-icon size="20px" name="fas fa-phone-alt" />
-            </span>
-            <a
-              class="text-white"
-              style="text-decoration: none; "
-              href="tel:084-323-2206"
-            >084-323-2206</a>
+          <div class="row">
+            <div>
+              <span class="q-pr-md">
+                <q-icon size="20px" name="fas fa-phone-alt" />
+              </span>
+              <a
+                class="text-white"
+                style="text-decoration: none; "
+                href="tel:02-026-0177"
+                >02-026-0177</a
+              >
+              (สำหรับนักเรียน)
+            </div>
+            <div class="q-ml-xl">
+              <span class="q-pr-md">
+                <q-icon size="20px" name="fas fa-phone-alt" />
+              </span>
+              <a
+                class="text-white"
+                style="text-decoration: none; "
+                href="tel:02-252-2489"
+                >02-252-2489</a
+              >
+              (สำหรับโรงเรียน)
+            </div>
           </div>
           <div>
             <span class="q-pr-md">
@@ -32,7 +49,8 @@
             <span
               class="cursor-pointer"
               onclick="window.open('https://www.facebook.com/WinnerEng', '_blank')"
-            >www.facebook.com/WinnerEng</span>
+              >www.facebook.com/WinnerEng</span
+            >
           </div>
           <div>
             <span class="q-pr-md">
@@ -42,7 +60,8 @@
               <span
                 lass="text-white"
                 onclick="window.open('https://info.winnerenglish@gmail.com', '_blank')"
-              >info.winnerenglish@gmail.com</span>
+                >info.winnerenglish@gmail.com</span
+              >
             </span>
           </div>
           <div class="row">
@@ -54,7 +73,9 @@
               <span
                 lass="text-white"
                 onclick="window.open('https://www.google.com/maps/place/%E0%B8%A7%E0%B8%B4%E0%B8%97%E0%B8%A2%E0%B8%B8%E0%B8%84%E0%B8%AD%E0%B8%A1%E0%B9%80%E0%B8%9E%E0%B8%A5%E0%B9%87%E0%B8%81%E0%B8%8B%E0%B9%8C/@13.7487136,100.5470682,17z/data=!3m1!4b1!4m5!3m4!1s0x30e29ec3baece621:0xfc8b407be8f878bb!8m2!3d13.7487136!4d100.5492569', '_blank')"
-              >1673/26 อาคารวิทยุคอมเพล็กซ์ ถนนเพชรบุรีตัดใหม่ แขวงมักกะสัน เขตราชเทวี 10400</span>
+                >1673/26 อาคารวิทยุคอมเพล็กซ์ ถนนเพชรบุรีตัดใหม่ แขวงมักกะสัน
+                เขตราชเทวี 10400</span
+              >
             </span>
           </div>
           <div>
